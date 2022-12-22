79113396f559648e6eb54feeab5d6793050ff08d
Avery <averycaldwell7@gmail.com>
2022-12-21 16:26:50 -0800

    updated weather object with values

--- Assets/js/clothing-script.js
+++ Assets/js/clothing-script.js
@@ -1,4 +1,4 @@
-// Variable to store the API key for shuttershock
+// letiable to store the API key for shuttershock
 const clothingKey =
   "v2/ZDNjMjJHOWc4OXpHVFVrdjJ4d3dnWTdKbmJyQXZIUEovMzY5OTA4NDk3L2N1c3RvbWVyLzQvYlpzdFB0TVZEMVp2VGpta2Q3NzM1LWNDS3ZOZTBNeWo5NFp3T0tBQVZCRk9RekZVQVlxa05tcENQSmtoRGZRNUhXZkpIX0VkSzlHbXZBel9PS2lNWGp4SjlUZFNKMDlUWGJPTEdVUGdVakszWm9qMkZjM3BMUjZuWWNIVTNrYjJuVGhneTJDZzQ1NW5kRjg4by1LOEJkQUMzb0k5TkY5UGIxWG1qVzZkQnBnQ0FnQUVkcXhtTkJGQWluY19PbWxtTWlaeS1ucGVYZmtCSnR2QW9GaHRKdy8ycmJrbXhETTdlUDJOOHF3eTlrRHR3";
 
@@ -10,22 +10,22 @@ let option5 = document.getElementById("option5");
 let option6 = document.getElementById("option6");
 
 // This function gets an image from the searched string
-var returnedImages;
+let returnedImages;
 function getImageObj(searchString) {
-  var myHeaders = new Headers();
+  let myHeaders = new Headers();
   myHeaders.append(
     "Authorization",
     "Bearer v2/ZDNjMjJHOWc4OXpHVFVrdjJ4d3dnWTdKbmJyQXZIUEovMzY5OTA4NDk3L2N1c3RvbWVyLzQvYlpzdFB0TVZEMVp2VGpta2Q3NzM1LWNDS3ZOZTBNeWo5NFp3T0tBQVZCRk9RekZVQVlxa05tcENQSmtoRGZRNUhXZkpIX0VkSzlHbXZBel9PS2lNWGp4SjlUZFNKMDlUWGJPTEdVUGdVakszWm9qMkZjM3BMUjZuWWNIVTNrYjJuVGhneTJDZzQ1NW5kRjg4by1LOEJkQUMzb0k5TkY5UGIxWG1qVzZkQnBnQ0FnQUVkcXhtTkJGQWluY19PbWxtTWlaeS1ucGVYZmtCSnR2QW9GaHRKdy8ycmJrbXhETTdlUDJOOHF3eTlrRHR3"
   );
 
-  var requestOptions = {
+  let requestOptions = {
     method: "GET",
     headers: myHeaders,
     redirect: "follow",
   };
   // fetches API url with specific parameters
   fetch(
-    "https://api.shutterstock.com/v2/images/search?category=fashion picture&per_page=500&query=" +
+    "https://api.shutterstock.com/v2/images/search?category=fashionpicture&per_page=500&query=" +
       searchString,
     requestOptions
   )
@@ -47,20 +47,23 @@ function randomImage() {
     returnedImages.data[randomNumber].assets.preview_1000.url;
 }
 
-// Object of weather types
+// This object holds the arrays of weather types
 weatherObj = {
-rain: "wet rainy waterproof",
-overcast: "overcast ",
-clouds: "clouds",
-snow: "snow",
-sun: "sun",
-mist: "mist",
-thunderstorm: "thunderstorm",
-clear sky: "clear sky",
-drizzle: "drizzle",
-clear: "clear",
+rain: ["rainy day outfit"],
+overcast: ["autumn clothing"],
+clouds: ["fall outfit", "cool outfit"],
+snow: ["warm outerwear", "winter clothing"],
+sun: ["summer outfit"],
+mist: ["rainy day clothing"],
+thunderstorm: ["windy rain clothes"],
+clear sky: ["summer clothes", "spring clothes"],
+drizzle: ["waterproof outfit",],
+clear: ["spring outfit"],
 };
 
+// TODO: fix
+  localStorage.setItem("weatherObj", weatherObj[i].toString());
+      weatherObj = localStorage.getItem("weaterObj").split(",");
 
 // have to call:
 // getImageObj("hot summer clothing")
